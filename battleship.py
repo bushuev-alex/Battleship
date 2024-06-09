cdaa668a01c0de8564ccf63fcfa12c6ced0a6ebb
bushuev-alex <bushuev-alex@mail.ru>
2024-06-09 23:44:29 +0700

    1. add level rate to each player 2. change player's names according to their levels 3. change draw_fields function 4. ShotCoordinates takes row and col instead of row-1 and col-1 5. print("THE SHIP IS KILLED!") now is in make_shot function 6. get_game_status print winner with its level

--- battleship.py
+++ battleship.py
@@ -8,10 +8,13 @@ from exceptions import *
 
 class Battleship:
 
-    def __init__(self, player1: str, player2: str):
+    def __init__(self, player_lvl: str, opponent_lvl: str):
         # create field objects and ships in them:
-        self.player1_name = player1.upper()
-        self.player2_name = player2.upper()
+        self.lvl1 = player_lvl
+        self.lvl2 = opponent_lvl
+
+        self.player1_name = f"1st ({player_lvl.upper()})"
+        self.player2_name = f"2nd ({opponent_lvl.upper()})"
 
         self.player1_field = Field()
         self.player1_field.generate_ships()
@@ -21,8 +24,8 @@ class Battleship:
 
     def create_players(self) -> tuple[Gamer, Gamer]:
         # create players
-        player1 = Player(self.player1_field, name=self.player1_name)
-        player2 = Player(self.player2_field, name=self.player2_name)
+        player1 = Player(self.player1_field, name=self.player1_name, level=self.lvl1)
+        player2 = Player(self.player2_field, name=self.player2_name, level=self.lvl2)
 
         player1.set_opponent(player2)
         player2.set_opponent(player1)
@@ -45,21 +48,19 @@ class Battleship:
         hiding_field = player.opponent.player_field.copy()
         hiding_field[hiding_field == "O"] = " "  # np.array
 
-        print(f"\n     {player.name} Field", ' ', ' ', ' ', ' ', f"   {player.opponent.name} Field")
-        print(' ', ' ', '1', '2', '3', '4', '5', '6', ' ', ' ', ' ', ' ', ' ', '1', '2', '3', '4', '5', '6')
-        print('  ---------------', ' ', ' ', '  ---------------')
-
+        print(f"\n", ' ' * 2, f"{player.name} Field", ' ' * 9, f"{player.opponent.name} Field")
+        print(' ' * 7, '1', '2', '3', '4', '5', '6', ' ' * 19, '1', '2', '3', '4', '5', '6')
+        print(' ' * 5, '---------------', ' ' * 15, '---------------')
         for n, rows in enumerate(zip(player.player_field, hiding_field)):
-            print(n + 1, '|', *rows[0], '|', ' ', ' ', n + 1, '|', *rows[1], '|')
-
-        print('  ---------------', ' ', ' ', '  ---------------')
+            print(' ' * 3, n + 1, '|', *rows[0], '|', ' ' * 13, n + 1, '|', *rows[1], '|')
+        print(' ' * 5, '---------------',  ' ' * 15, '---------------')
 
     @staticmethod
     def get_new_coordinates(player: Gamer) -> ShotCoordinates:
         while True:
             try:
                 row, col = player.ask_coordinates()
-                return ShotCoordinates(row - 1, col - 1)
+                return ShotCoordinates(row, col)
             except CoordinatesException as e:
                 print(e)
             except BusyCellOnFieldException as e:
@@ -68,9 +69,9 @@ class Battleship:
                 print(e)
 
     @staticmethod
-    def check_ship_is_live(coordinates: ShotCoordinates, opponent: Gamer) -> None:
-        row = coordinates.row
-        col = coordinates.col
+    def check_ship_is_dead(coordinates: ShotCoordinates, opponent: Gamer) -> bool:
+        row = coordinates.row - 1
+        col = coordinates.col - 1
         for ship in opponent.ships:
             # find right ship:
             condition = (row in range(ship.coordinates.row_start, ship.coordinates.row_end + 1) and
@@ -82,26 +83,31 @@ class Battleship:
                 opponent.player_field[ship.coordinates.row_start:ship.coordinates.row_end + 1,
                                       ship.coordinates.col_start:ship.coordinates.col_end + 1] = "■"
                 ship.is_killed = True
-                print("THE SHIP IS KILLED!")
-                break
+                return True
+        return False
 
-    def make_shot(self, coordinates: ShotCoordinates, player: Gamer) -> bool:
-        row = coordinates.row
-        col = coordinates.col
+    def make_shot(self, coordinates: ShotCoordinates, player: Gamer) -> [ShotCoordinates|bool]:
+        row = coordinates.row - 1
+        col = coordinates.col - 1
         if player.opponent.player_field[row, col] == "O":
             player.opponent.player_field[row, col] = "X"
             print("THE SHIP IS DAMAGED")
-            self.check_ship_is_live(coordinates, player.opponent)
-            return True
+            if self.check_ship_is_dead(coordinates, player.opponent):
+                print("THE SHIP IS KILLED!")
+                player.successful_shot = None
+                return coordinates
+            player.successful_shot = coordinates
+            return coordinates
         else:
             player.opponent.player_field[row, col] = "*"
+            player.successful_shot = None
             print("OVERSHOT!")
             return False
 
     @staticmethod
-    def get_game_status(user_player: Gamer, ai_player: Gamer) -> str:
-        if all((ship.is_killed for ship in user_player.ships)):
-            return "AI WINS!"
-        if all((ship.is_killed for ship in ai_player.ships)):
-            return "USER WINS!"
+    def get_game_status(player_1: Gamer, player_2: Gamer) -> str:
+        if all((ship.is_killed for ship in player_1.ships)):
+            return f"{player_2.name} WINS!"
+        if all((ship.is_killed for ship in player_2.ships)):
+            return f"{player_1.name} WINS!"
         return "Game not finished"
